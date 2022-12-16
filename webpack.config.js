532391f25e8a231367227377035805db3071da4a
Dila <kimdilany@gmail.com>
2022-12-17 03:59:00 +0500

    new update. not finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+"use strict";
 const path = require("path");
 module.exports = {
   mode: "development",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
+    },
+    historyApiFallback: {
+      rewrites: [{ from: /info\/?.*/, to: "/info.html" }],
     },
   },
   entry: "./index.js",
@@ -12,5 +16,23 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 
+  // devServer: {
+  //   allowedHosts: [
+  //     'host.com',
+  //     'subdomain.host.com',
+  //     'subdomain2.host.com',
+  //     'host2.com',
+  //   ],
+  // },
+  // historyApiFallback: true
+
   // watch: true,
+  // html loaders test
+  // test: /\.html$/,
+  // use: ["html-loader"],
+  // https: true,
+  // proxy: {
+  //   "/api": "http://localhost:8081/",
+  // },
+  // server: "https",
 };
